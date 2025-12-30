5429e493299d4e83a3171bd0b9f780f1ee961496
Sahil kumar <sahilsync07@gmail.com>
2025-12-30 09:19:59 +0530

    chore: update supabase credentials

--- frontend/src/utils/supabase.js
+++ frontend/src/utils/supabase.js
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://zyupasrvqqomsowleqyk.supabase.co"; // Replace with your Supabase project URL
 const supabaseAnonKey =
-  "sb_publishable_A_0SkVmCBg2oQwEZO9MyVw_oeLG4Rez"; // Replace with your Supabase anon key
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5dXBhc3J2cXFvbXNvd2xlcXlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNTE3ODMsImV4cCI6MjA4MjYyNzc4M30.RuTivvXa17lssxYM0z1C4dHIq2NcNUCsxPEe9pyw3qU"; // Replace with your Supabase anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
